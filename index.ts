9c2c678385c997c83ea81c4bdc76477463a72170
mehedii-h0 <mehedii.h03@gmail.com>
2025-05-08 06:54:41 +0600

    Problem 1 resolve

---
+++ index.ts
@@ -0,0 +1,10 @@
+function formatString(input: string, toUppercase?: boolean): string {
+  if (toUppercase) {
+    return input.toUpperCase();
+  } else {
+    return input.toLowerCase();
+  }
+}
+
+
+
